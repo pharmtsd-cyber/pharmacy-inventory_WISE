00dcfa51366e628c1a3cdef672185a8cd2cdfded
pharmachris <pharm.tsd@gmail.com>
2026-06-01 11:26:28 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,6 @@
 
 // 🌟 1. 自動讀取網址列後方的參數
 const urlParams = new URLSearchParams(window.location.search);
-// 💡 修改：拔除預設 'admin'，讓系統可以停在大廳
 export const currentDept = urlParams.get('dept');
 
 // 🌟 2. 各單位的「GAS 後端網址」對照表
@@ -14,6 +13,16 @@ const BACKEND_MAP = {
   opd: "https://script.google.com/macros/s/AKfycbymGL_J04TEDCFV7xH-M5-zCYmgj8BAtz6WVE06m3MR4Sl1rZ1SGs6ALDyAlWbsoDmHBg/exec", 
   erd: "https://script.google.com/macros/s/AKfycbyMTHmPUdKmhyLO4Gm-OPKuYyAFpclId_bwFcSAUsAIt4QKLlMCXpUOGXBI1MPfxmKg/exec", 
   comp: "https://script.google.com/macros/s/AKfycbwQzl8Cbi6ZZtcMDcnm50m-5L6771mJyxIVoX_d_yIlgA7QJ3iN0ss_RonUBZeHuzFhig/exec" 
+};
+
+// 🌟 🚀 新增：各單位專屬的「盤點表維護連結」對照表
+// 請將下方的網址替換成各藥局真實的 Google 表單或試算表連結
+export const MAINTENANCE_LINK_MAP = {
+  admin: "https://docs.google.com/spreadsheets/d/您的運管組維護網址", // 運管組
+  ipd: "https://docs.google.com/spreadsheets/d/1HXOY5vYQogaruF8hdSr9WqhIhBESYJh7f0l7plR4azs/edit?usp=sharing", // 住院藥局
+  opd: "https://docs.google.com/spreadsheets/d/19FeUxkDPYJ-G_ggj_HjifKUTWxVfVn30QwZdX8WVIsg/edit?usp=sharing", // 門診藥局
+  erd: "https://docs.google.com/spreadsheets/d/1sTt9kEmYgyVcUvEwwJWemo1dFImF6sqK4QCOiEZCvO0/edit?usp=sharing", // 急診藥局
+  comp: "https://docs.google.com/spreadsheets/d/1OoWvEidJVldKy75HI6mwbiaGzyeiw_VP7I0RDgMsuE4/edit?usp=sharing" // 調配藥局
 };
 
 // 🌟 3. 各單位的「顯示名稱」對照表
